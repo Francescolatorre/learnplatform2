77a43c146300a8d41b3a6a57f7b2b10deade1b60
Francesco La Torre <Francesco.latorre@me.com>
2025-06-16 16:16:24 +0200

    Fix middleware list formatting

--- learningplatform_backend/settings.py
+++ learningplatform_backend/settings.py
@@ -97,12 +97,11 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",iddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",e",
+    "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # Consolidated middlewareacking.XFrameOptionsMiddleware",
     "learningplatform_backend.middleware.middleware.RequestLoggingMiddleware",
     "learningplatform_backend.middleware.middleware.DebugLoggingMiddleware",
 ]
